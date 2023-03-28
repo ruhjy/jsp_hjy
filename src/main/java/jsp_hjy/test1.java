f61ca3a0622b9d02fa2fc38a8b9711316d848b22
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-28 09:57:45 +0900

    ??

---
+++ src/main/java/jsp_hjy/test1.java
@@ -0,0 +1,16 @@
+package jsp_hjy;
+
+public class test1 {
+	public static void main(String[] args) {
+		
+		
+		int i = (int)Math.ceil(3.8);
+		System.out.println("i : " + i);
+		
+		System.out.println(Integer.valueOf((int) Math.ceil(3.8)));
+		
+		Double
+		Short
+		short
+	}
+}
