77dd4ed61f61c41f77411e8e589b8eeb5e467131
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-31 16:01:01 +0900

    ??

--- src/main/java/jsp_hjy/test1.java
+++ src/main/java/jsp_hjy/test1.java
@@ -4,22 +4,30 @@ import java.util.*;
 
 public class test1 {
 	public static void main(String[] args) {
-		
-		
-		int i = (int)Math.ceil(3.8);
+
+		int i = (int) Math.ceil(3.8);
 		System.out.println("i : " + i);
-		
+
 		System.out.println(Integer.valueOf((int) Math.ceil(3.8)));
-		
-		String[] hobbys = {"java", "html"};		
-		
-		
-		for(String hobby : hobbys) {
+
+		String[] hobbys = { "java", "html" };
+
+		for (String hobby : hobbys) {
 			System.out.println(hobby);
 		}
+
+		String[] movies = { "java", "html" };
+		Arrays.stream(movies).forEach(movie -> System.out.print("<h1>movie </h1>"));
+
+	}
+	
+	static class TestSingleton {
+		private static final test1 instance = new test1();
 		
+		private TestSingleton() { }
 		
-		String[] movies = {"java", "html"};
-			Arrays.stream(movies).forEach(movie -> System.out.print("<h1>movie </h1>"));
+		public static test1 getInstance() {
+			return instance;
+		}
 	}
 }
